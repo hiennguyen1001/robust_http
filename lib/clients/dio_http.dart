931cafb405cbe9123fa7b768c89174305eaa08d3
Hien nguyen <vhnguyen1001@gmail.com>
2025-01-03 09:55:20 +0700

    remove setting adapter explicitly

--- lib/clients/dio_http.dart
+++ lib/clients/dio_http.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
-import 'package:dio/browser.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/foundation.dart';
+import 'package:native_dio_adapter/native_dio_adapter.dart';
 import 'package:robust_http/clients/base_http.dart';
 import 'package:robust_http/exceptions.dart';
 import 'package:robust_http/file_info.dart';
@@ -56,20 +56,7 @@ class DioHttp extends BaseHttp {
     }
 
     _dio = new Dio(baseOptions);
-    HttpClientAdapter adapter;
-    if (kIsWeb) {
-      adapter = BrowserHttpClientAdapter();
-    } else {
-      final client = HttpClient()
-        ..idleTimeout = Duration(seconds: options["idleTimeout"] ?? 3);
-
-      if (options["maxConnectionsPerHost"] != null) {
-        client.maxConnectionsPerHost = options["maxConnectionsPerHost"];
-      }
-      adapter = IOHttpClientAdapter()
-        ..createHttpClient = () => client;
-    }
-    _dio.httpClientAdapter = adapter;
+    _dio.httpClientAdapter = NativeAdapter();
     var logLevel = options['logLevel'];
     if (logLevel != 'none') {
       _dio.interceptors.add(LoggerInterceptor(logLevel == 'debug'));
